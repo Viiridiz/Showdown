b0f09154b7dc5260ad951b97d922aaad3f26441e
Viiridiz <amashkashareef@gmail.com>
2026-04-20 19:19:32 -0400

    Reworked frontend folder to be React instead of Vanilla

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1 style={{ color: 'var(--poke-red)' }}>Showdown</h1>
+      <p>The frontend is alive.</p>
+      <button style={{ marginTop: '1rem' }}>Test Button</button>
+    </div>
+  )
+}
+
+export default App;
